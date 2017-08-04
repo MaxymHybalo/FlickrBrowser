a2711baca972c0fb82599e932990e64f938fbc64
esdth <keepass.log@gmail.com>
2017-08-04 15:34:41 +0300

    Fetched album photo preview

--- src/app/profile/album-list/album-preview/album-preview.component.ts
+++ src/app/profile/album-list/album-preview/album-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AlbumService } from '../album.service'
 
 @Component({
   selector: 'app-album-preview',
@@ -10,9 +11,25 @@ export class AlbumPreviewComponent implements OnInit {
 
   @Input() album: object;
 
-  constructor() { }
+  photo = null;
+  photoResource = null;
+
+  constructor(private service: AlbumService) { }
 
   ngOnInit() {
+    this.service.getPhotos(this.album)
+      .then(photoset => {
+        this.photo = photoset;
+        this.service.getPhotoSizes(photoset['photoset']['photo'][0]['id'])
+          .then(sizes => {
+            this.mapResource(sizes)
+          }
+        )
+      })
   }
 
+  private mapResource(resorce) {
+    let sizes = resorce['sizes']['size'];
+    this.photoResource = sizes[2];
+  }
 }
--- src/app/profile/album-list/album.service.ts
+++ src/app/profile/album-list/album.service.ts
@@ -5,22 +5,63 @@ import api from '../../../api'
 
 import 'rxjs/add/operator/toPromise';
 
+
+
+
 @Injectable()
-export class AlbumService {
+export class AlbumService{
     constructor(private http: Http) {}
-    
+    accessToken = null;
+    basicAPIParams = null;
+
     getAlbums(): Promise<object> {
-        let accessToken = JSON.parse(localStorage.getItem('accessToken'));
+        this.init()
         let params = {
-            method: 'flickr.photosets.getList',
-            format: 'json',
-            nojsoncallback: 1,
-            oauth_token: accessToken['oauth_token']
+            method: 'flickr.photosets.getList'    
         }
-        return this.http.get(buildURL(params,api['REST'], accessToken['oauth_token_secret']))
+        params = Object.assign(this.basicAPIParams, params);
+        return this.http.get(buildURL(params,api['REST'], this.accessToken['oauth_token_secret']))
             .toPromise()
             .then(response => response.json() as object)
             .catch(error => error);
 
+    }
+
+    getPhotos(photoset, page=1, per_page=1): Promise<object>{
+        this.init()
+        let params = {
+            method: 'flickr.photosets.getPhotos',
+            photoset_id: photoset.id,
+            user_id: decodeURIComponent(this.accessToken['user_nsid']),
+            page: page,
+            per_page: per_page
+        }   
+        params = Object.assign(this.basicAPIParams, params);
+        return this.http.get(buildURL(params, api['REST'], this.accessToken['oauth_token_secret']))
+            .toPromise()
+            .then(response => response.json() as object)
+            .catch(error => error)
+    }
+
+    getPhotoSizes(photoId): Promise<object> {
+        this.init();
+        let params = {
+            method: 'flickr.photos.getSizes',
+            photo_id: photoId
+        }
+        params = Object.assign(params, this.basicAPIParams);
+        return this.http.get(buildURL(params, api['REST'], this.accessToken['oauth_token_secret']))
+            .toPromise()
+            .then(response => response.json() as object)
+            .catch(error => error)
+    }
+
+    private init(): void { 
+        this.accessToken = JSON.parse(localStorage.getItem('accessToken'));
+        this.basicAPIParams = {
+            format: 'json',
+            nojsoncallback: 1,
+            oauth_token: this.accessToken['oauth_token']
+        } 
     }
 }
