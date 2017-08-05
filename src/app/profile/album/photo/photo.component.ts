c6d7c204d6c47b6214018984c89c9789ecd480c5
esdth <keepass.log@gmail.com>
2017-08-05 16:27:09 +0300

    Show album preview, styled preview

--- src/app/profile/album/photo/photo.component.ts
+++ src/app/profile/album/photo/photo.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AlbumService } from '../../album.service'
+
 
 @Component({
   selector: 'app-photo',
@@ -9,9 +11,14 @@ export class PhotoComponent implements OnInit {
 
   @Input() photo: object;
 
-  constructor() { }
+  sizes: object;
+
+  constructor(private service: AlbumService) { }
 
   ngOnInit() {
+    this.service.getPhotoSizes(this.photo['id'])
+      .then(sizes => this.sizes = sizes['sizes']['size'][3])
   }
+
 
 }
